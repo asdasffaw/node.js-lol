17ee2bea5fa5936543c408f5393e17f10e885431
asdasffaw <147673902+asdasffaw@users.noreply.github.com>
2023-11-01 15:02:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+import express from "express";
+import os from "node:os";
+
 const request = require("request");
-const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const hostname = os.hostname();
+const app = express();
+const port = 3000;
 
-request('google.com', function (error, response, html) {
+
+
+
+app.get("/", async (req, res) => {
+	request('google.com', function (error, response, html) {
     if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
         // Get text 
-        console.log("------- with request module -------")
-        console.log($.text());
+        res.send($.text());
         // Get HTML 
         //console.log($.html());
     }
+});
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
 });
