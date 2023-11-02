49f03fa3a60aa0b43193c82603e72ecbbaf8dd73
asdasffaw <147673902+asdasffaw@users.noreply.github.com>
2023-11-02 14:25:27 -0500

    Update indexp.js

--- indexp.js
+++ indexp.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const httpProxy = require('http-proxy');
-
-// Create a proxy and listen on port 3000
-const proxy = httpProxy.createProxyServer({});
-const app = express();
-app.get('*', function(req, res) {
-  // Prints "Request GET https://httpbin.org/get?answer=42"
-  console.log('Request', req.method, req.url);
-  proxy.web(req, res, { target: `${req.protocol}://${req.hostname}` });
-});
-const server = await app.listen(3000);
+var http = require('http'),
+    httpProxy = require('http-proxy');
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({target:'http://localhost:9000'}).listen(8000); // See (†)
+ 
+//
+// Create your target server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(9000);
