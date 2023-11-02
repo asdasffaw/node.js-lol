7dbb812fdd2f92ffab551d9f7f8df69afcb0b10f
asdasffaw <147673902+asdasffaw@users.noreply.github.com>
2023-11-02 14:17:11 -0500

    Create indexp.js

---
+++ indexp.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const httpProxy = require('http-proxy');
+
+// Create a proxy and listen on port 3000
+const proxy = httpProxy.createProxyServer({});
+const app = express();
+app.get('*', function(req, res) {
+  // Prints "Request GET https://httpbin.org/get?answer=42"
+  console.log('Request', req.method, req.url);
+  proxy.web(req, res, { target: `${req.protocol}://${req.hostname}` });
+});
+const server = await app.listen(3000);
